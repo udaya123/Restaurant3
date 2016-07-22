41ac8713da8967ed4028c8122e640e25185095f1
udayaratna <udaya.ratna@gmail.com>
2016-07-22 15:11:54 -0700

    added testing code

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
-
-RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Restaurant do
+  it 'validates the presence of name' do
+    restaurant = Restaurant.new
+    restaurant.valid?
+    expect(restaurant.errors.messages[:name]).to include "can't be blank"
+  end
 end
+
+
+
