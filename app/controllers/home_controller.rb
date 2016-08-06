21843ccae772eb028b352ca7e87ea88860ffa3fc
udayaratna <udaya.ratna@gmail.com>
2016-08-05 23:17:13 -0700

    redirected users to restaurant at login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+   if user_signed_in?  
     redirect_to user_restaurants_path(current_user)
-  end
+   else
+   redirect_to new_user_session_path 
+   end
+end
 end
